f1509f9dbcec6abf9feab496f84154d340ea0eb8
FoonsGabriel <fonsecagabriel2327@gmail.com>
2025-09-03 20:24:24 -0300

    Atualizei o main.py
    mandarão eu fazer

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ print('Olá ' + nome)
 print('Deixe uma mensagem salva')
 mensagem = input('Digite a mensagem: ')
 print(nome + ' deixou a seguinte mensagem:\n' + mensagem)
+
+def idade(num):
+    if num < 18:
+        print('Menor de idade')
+    else:
+        print('Maior de idade')
+
+num = int(input('Qual sua idade?'))
+print (idade(num))
